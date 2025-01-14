59bed267d8837aa356887a18b0a7daa81299bf6b
Priyadhar29 <144304614+Priyadhar29@users.noreply.github.com>
2025-01-14 19:02:22 +0530

    Update Booking.java

--- Taxi Booking System/Booking.java
+++ Taxi Booking System/Booking.java
@@ -1,13 +1,14 @@
-public class Booking {
-    int bookingid,customerid, pickuptime, droptime, amount;
-    char from,to;
-    public Booking(int bookingid, int customerid, char from, char to, int pickuptime , int droptime, int amount) {
-        this.bookingid = bookingid;
-        this.customerid = customerid;
+class Booking {
+    int bookingId, customerId, pickupTime, dropTime, amount;
+    char from, to;
+
+    public Booking(int bookingId, int customerId, char from, char to, int pickupTime, int dropTime, int amount) {
+        this.bookingId = bookingId;
+        this.customerId = customerId;
         this.from = from;
         this.to = to;
-        this.pickuptime = pickuptime;
-        this.droptime = droptime;
+        this.pickupTime = pickupTime;
+        this.dropTime = dropTime;
         this.amount = amount;
     }
 }
